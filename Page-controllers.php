55159dc3c1acc47b65bff0b55113d0c3087cbd5a
Onur YILMAZ <64357484+onuryilmaz-yzmedia@users.noreply.github.com>
2023-02-07 10:37:31 +0300

    Create Page-controllers.php
    parse controllers

---
+++ Page-controllers.php
@@ -0,0 +1,35 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Page extends MY_Controller {
+	public function __construct()
+    {
+        parent::__construct();
+        $this->load->model("page_model");
+        $this->load->library('parser');
+    }
+
+    public function page_detail($link=false){
+        $content = $this->page_model->get_content($link); 
+        if(isset($content)){
+                $keywords = $this->page_model->get_keywords($content->id);
+                $content_gallery = $this->page_model->page_gallery($content->id);
+                if($content->resim <> "") {$page_image = '<img class="page-image" src="images/'.$page->image.'">';}else{$page_image="";}
+                $data = array(
+                    'id'               => $content->id,
+                    'name'              => $content->name,
+                    'content'           => $content->content,
+                    'image'            => $page_image,
+                    'link'             => $content->link,
+                    'meta_description' => $content->meta_description,
+                    'seo_title'        => $content->seo_title,
+                    'keywords'         => $keywords->result_array(),
+                    'icerik_gallery'   => $content_gallery->result_array(),
+                );
+                $this->parser->parse('page-template', $data);
+            }else{
+                $this->load->view("404.html");
+            }
+    }
+
+}
